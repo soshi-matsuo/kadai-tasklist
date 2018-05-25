bb2cd478142602f0fb5bde1ce9c30af81a079350
soshi.matsuo <soshi.matsuo@rakuten.com>
2018-05-25 07:56:09 +0000

    CRUD tasks

---
+++ resources/views/Tasks/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- Write content for each page here -->
+<h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{ $task->content }}</li>
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+{!! link_to_route('tasks.create', '新規タスクの追加') !!}
+@endsection
